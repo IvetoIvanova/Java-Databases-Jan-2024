ad0616fbc808054b00472146a1a3a711eec26eae
IvetoIvanova <135642648+IvetoIvanova@users.noreply.github.com>
2024-01-17 12:31:42 +0200

    Added solution related to the 'MySQL' module.
    Added  a solution for a task within the 'MySQL' module of the "Java-Databases-Jan-2024" course, specifically addressing the Basic-CRUD. The corresponding solution can be found in the 'Exercise' folder under the '02Basic-CRUD' topic, with the solution named '16CreateViewEmployeesWithJobTitles.sql'.

---
+++ MySQL/02Basic-CRUD/Exercise/16CreateViewEmployeesWithJobTitles.sql
@@ -0,0 +1,11 @@
+CREATE VIEW v_employees_job_titles AS
+    SELECT 
+        CONCAT(first_name,
+                ' ',
+                IF(middle_name IS NOT NULL,
+                    CONCAT(middle_name, ' '),
+                    ''),
+                last_name) AS full_name,
+        job_title
+    FROM
+        employees;
