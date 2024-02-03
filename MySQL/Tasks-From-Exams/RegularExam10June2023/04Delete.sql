f579e708bb547667a0e04ef0ba5b7d5edfd8a752
IvetoIvanova <135642648+IvetoIvanova@users.noreply.github.com>
2024-02-03 13:17:23 +0200

    In this commit, I've included solutions for 2 tasks from the 'RegularExam10June2023' topic of the 'MySQL' module.
    The corresponding solutions can be found in the 'Tasks-From-Exams' folder under the 'RegularExam10June2023' folder, with the solutions named: '04Delete.sql' and '05Cities.sql'.

---
+++ MySQL/Tasks-From-Exams/RegularExam10June2023/04Delete.sql
@@ -0,0 +1,5 @@
+DELETE FROM universities 
+WHERE
+    number_of_staff IS NULL;
+
+-- SET SQL_SAFE_UPDATES = 0;
---
+++ MySQL/Tasks-From-Exams/RegularExam10June2023/05Cities.sql
@@ -0,0 +1,7 @@
+-- SELECT * FROM universities_db.cities;
+
+SELECT 
+    *
+FROM
+    cities
+ORDER BY population DESC;
