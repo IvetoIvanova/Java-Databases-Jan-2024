34dd512a6c7d8fc66101649c7ca12256e2986916
IvetoIvanova <135642648+IvetoIvanova@users.noreply.github.com>
2024-02-05 14:39:24 +0200

    In this commit, I've included solutions for 2 tasks from the 'RegularExam15Oct2022' topic of the 'MySQL' module.
    The corresponding solutions can be found in the 'Tasks-From-Exams' folder under the 'RegularExam15Oct2022' folder, with the solutions named: '02Insert.sql' and '03Update.sql'.

---
+++ MySQL/Tasks-From-Exams/RegularExam15Oct2022/02Insert.sql
@@ -0,0 +1,11 @@
+-- SELECT COUNT(*) FROM products;
+
+INSERT INTO products(name, type, price)
+(
+SELECT 
+	CONCAT(last_name, ' specialty'),
+	'Cocktail',
+    CEILING(0.01 * salary)
+FROM waiters
+WHERE id > 6
+);
