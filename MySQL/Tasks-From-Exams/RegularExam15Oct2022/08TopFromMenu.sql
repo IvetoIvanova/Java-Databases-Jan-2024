fce25625084cf801f9eeba278caec61e4baf8fda
IvetoIvanova <135642648+IvetoIvanova@users.noreply.github.com>
2024-02-05 16:02:58 +0200

    In this commit, I've included solutions for 2 tasks from the 'RegularExam15Oct2022' topic of the 'MySQL' module.
    The corresponding solutions can be found in the 'Tasks-From-Exams' folder under the 'RegularExam15Oct2022' folder, with the solutions named: '08TopFromMenu.sql' and '09Availability.sql'.

---
+++ MySQL/Tasks-From-Exams/RegularExam15Oct2022/08TopFromMenu.sql
@@ -0,0 +1,9 @@
+SELECT 
+    id, `name`, COUNT(op.order_id) AS `count`
+FROM
+    products AS p
+        JOIN
+    orders_products AS op ON op.product_id = p.id
+GROUP BY p.id
+HAVING `count` >= 5
+ORDER BY `count` DESC , `name` ASC;
