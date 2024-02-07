08ee79d4461e97486f5c44ed0c8304778d1ce08d
Ivelina Ivanova <135642648+IvetoIvanova@users.noreply.github.com>
2024-02-07 15:37:03 +0200

    Update 10NumberOfFlights.sql
    Тhe lines are formatted.

--- MySQL/Tasks-From-Exams/DatabaseBasicsMySQLRetakeExam11Dec2022/10NumberOfFlights.sql
+++ MySQL/Tasks-From-Exams/DatabaseBasicsMySQLRetakeExam11Dec2022/10NumberOfFlights.sql
@@ -4,13 +4,13 @@ CREATE FUNCTION udf_count_flights_from_country(country VARCHAR(50))
 RETURNS INT
 DETERMINISTIC
 BEGIN
-	DECLARE result INT;
+    DECLARE result INT;
     SET result := (SELECT 
-					COUNT(f.departure_country) AS flights_count
-					FROM flights f
-					JOIN countries c ON f.departure_country = c.id
-					WHERE c.name = country);
-	RETURN result;
+		COUNT(f.departure_country) AS flights_count
+		FROM flights f
+		JOIN countries c ON f.departure_country = c.id
+		WHERE c.name = country);
+    RETURN result;
 END $
 DELIMITER ;
 
