0fb55a2c634f54941e1823ca42bc024dc3ada9fe
IvetoIvanova <135642648+IvetoIvanova@users.noreply.github.com>
2024-02-28 19:32:18 +0200

    Added 'T04HospitalDatabase' folder to the 'Spring Data' module.
    The corresponding folder can be found in the '04Hibernate-Code-First' folder under the 'Exercise' folder.

---
+++ SpringData/04Hibernate-Code-First/Exercise/T04HospitalDatabase/src/main/java/entity/Medicament.java
@@ -0,0 +1,16 @@
+package entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import java.util.Set;
+
+@Entity
+@Table(name = "medicaments")
+public class Medicament extends BaseEntity {
+    @Column
+    private String name;
+    @ManyToMany(mappedBy = "medicaments")
+    private Set<Diagnose> diagnoses;
+}
---
+++ SpringData/04Hibernate-Code-First/Exercise/T04HospitalDatabase/src/main/java/entity/Visitation.java
@@ -0,0 +1,21 @@
+package entity;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Set;
+
+@Entity
+@Table(name = "visitations")
+public class Visitation extends BaseEntity {
+    @Column
+    private LocalDate date;
+    @Column
+    private String comments;
+
+    @ManyToOne
+    @JoinColumn(name = "patient_id", referencedColumnName = "id")
+    private Patient patient;
+    @ManyToOne
+    @JoinColumn(name = "diagnose_id", referencedColumnName = "id")
+    private Diagnose diagnose;
+}
