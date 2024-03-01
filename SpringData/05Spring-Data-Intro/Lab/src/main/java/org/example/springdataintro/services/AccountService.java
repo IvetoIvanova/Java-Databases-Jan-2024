eeb910a99562a4476b617491609a4d47c968e073
Ivelina Ivanova <135642648+IvetoIvanova@users.noreply.github.com>
2024-03-01 11:48:06 +0200

    Update AccountService.java
    Formatted the code for better readability.

--- SpringData/05Spring-Data-Intro/Lab/src/main/java/org/example/springdataintro/services/AccountService.java
+++ SpringData/05Spring-Data-Intro/Lab/src/main/java/org/example/springdataintro/services/AccountService.java
@@ -5,7 +5,9 @@ import org.example.springdataintro.models.Account;
 import java.math.BigDecimal;
 
 public interface AccountService {
+    
     void createAccount(Account account);
+    
     void withdrawMoney(BigDecimal money, Long id);
 
     void depositMoney(BigDecimal money, Long id);
