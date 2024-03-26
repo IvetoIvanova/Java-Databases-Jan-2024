9fa5a144face2fb6dc5a1ab57f890b65f7771dcf
Ivelina Ivanova <135642648+IvetoIvanova@users.noreply.github.com>
2024-03-26 11:52:47 +0200

    Update BorrowingRecordRepository.java
    Added findAllByBookGenreAndOrderByBorrowDateDesc()

--- SpringData/Java-Databases-Advanced-Exams/LibraryManagementSystem/src/main/java/softuni/exam/repository/BorrowingRecordRepository.java
+++ SpringData/Java-Databases-Advanced-Exams/LibraryManagementSystem/src/main/java/softuni/exam/repository/BorrowingRecordRepository.java
@@ -7,4 +7,9 @@ import softuni.exam.models.entity.BorrowingRecord;
 @Repository
 public interface BorrowingRecordRepository extends JpaRepository<BorrowingRecord, Long> {
 
+    @Query(value = "SELECT br FROM BorrowingRecord br " +
+            "JOIN Book b ON br.book.id = b.id " +
+            "WHERE b.genre = 'SCIENCE_FICTION' AND br.borrowDate < '2021-09-10' " +
+            "ORDER BY br.borrowDate DESC")
+    Set<BorrowingRecord> findAllByBookGenreAndOrderByBorrowDateDesc();
 }
